f03ecc36c18747e41aba80f03c47a3d31b325cde
So <1979510177@qq.com>
2017-03-22 00:26:02 +0800

    get tf-idf and make a rel dictionary

--- offline/getPathTf.py
+++ offline/getPathTf.py
@@ -21,21 +21,35 @@ with open(r"./txt/dist/my/paths_tf.txt",'w',encoding='utf-8') as dist:
     PSstrArr=[] #抹平后的一维数组
     for index,item in enumerate(PS):
       PSlstArr.append( list(set(eval(item))) ) # str to lsit,然后去重
-
+    # print(PSlstArr)
     s=''
     for lstItem in PSlstArr:
-      s+='\t'.join(lstItem) 
+      s+='\t'+'\t'.join(lstItem)  #最前面多了一个'\t'
+    s=s[1:]
+    # print(s)
     PSstrArr=s.split('\t')
+    # print(PSstrArr)
     m=int(lst[2])
 
-    times=0
+    
     PS=[]
     for p in PSstrArr:
+      times=0
       for pArr in PSlstArr:
         if p in pArr:
           times=times+1
+      # print(p+" %d" % (times))
       tf=round(times/m,8)
-      PS.append(str([p,tf]))
+      if abs(tf-0)<0.00000001:
+        print(p)
+        print(times)
+        print(tf)
+      #存在[['write',0.5],['write',0.5],['write',0.5]]
+      #的情况，但在前面还不能去重，因为要统计多少个数组中出现过write
+      #现在写个函数去重
+      res=str([p,tf])
+      if res not in PS:
+        PS.append(res)
     dist.write(pid+'\t\t\t'+str(PS)+'\n')
     # print(PS)
 
--- offline/getPathTfIdf.py
+++ offline/getPathTfIdf.py
@@ -1,2 +1,67 @@
 # 从path_tf读取数据，计算idf，然后计算tf-idf，再降序排列
-# 
+# ' ;'连接的是谓词路径
+import math
+import json
+def isIn2DArr(s,arr):
+  """s是否出现在二维数组中过
+  但这里只是二维数组的第0个有用"""
+  for lst in arr:
+    if s==lst[0]:
+      return True
+  return False
+def getShowTimes(s):
+  """获取s在dic多少数组中出现过"""
+  count=0
+  for key in dic:
+    if isIn2DArr(s,dic[key]):
+      count=count+1
+  return count
+
+
+file = open(r'./txt/dist/my/paths_tf.txt', 'r', encoding='utf-8')
+line=True
+i=0
+dic={}
+with open(r"./txt/dist/my/paths_tf—idf.txt",'w',encoding='utf-8') as dist:
+  while line:
+    line=file.readline()
+
+    if line=='\n' or line=='':
+      continue
+    lst=line[:-1].split('\t\t\t')
+    # print(lst)
+    if lst[0] not in dic:
+      arr=eval(lst[1])
+      arr=list(map(lambda s:eval(s),arr))
+      dic[lst[0]]=arr
+
+    # print(line)
+    # print(lst)
+    # print(dic)
+    # break
+  print('while end')
+  # pattern 的总数
+  countT=len(dic)
+  print(countT)
+  for key in dic:
+    pathArr=dic[key]
+    for lst in pathArr:
+      i+=1
+      #['producer', 0.01492537],path ,tf
+      times=getShowTimes(lst[0])
+      idf=round(math.log(countT/(times+1)),8)
+      # lst.append(idf)
+      confidence=round(lst[1]*idf,8)
+      # 查看异常
+      if abs(confidence-0)<0.00000001:
+        print(lst[0])
+        print(confidence)
+      # lst.append(confidence)
+      lst[1]=confidence
+      if i%100==0:
+        print(i)
+    # 排序
+    dic[key]=sorted(pathArr,key=(lambda x:x[1]), reverse=True)
+  # print(dic)
+  json.dump(dic,dist)
+
